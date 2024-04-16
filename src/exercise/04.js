d294305e4a744edc58f0e1d97d2d85e7ffd9b657
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-16 10:27:32 -0600

    react hooks

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,7 +5,7 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
