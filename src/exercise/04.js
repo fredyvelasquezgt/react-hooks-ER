1ae55afa974a4eef087f889510885beb64f546f0
Fredy Velásquez <macacoloco696@gmail.com>
2024-04-16 10:40:15 -0600

    react hooks

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -11,26 +11,14 @@ function Board() {
   const nextValue = calculateNextValue(squares)
   const winner = calculateWinner(squares)
   const status = calculateStatus(winner, squares, nextValue)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
+ 
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
-    // 🐨 first, if there's already a winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+    const squaresCopy = [...squares]
+    squaresCopy[squares] = nextValue;
+    setSquares(squaresCopy)
   }
 
   function restart() {
